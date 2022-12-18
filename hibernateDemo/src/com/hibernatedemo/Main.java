049b80cc5fd66c86680257f179a7d0ab2e9d70a1
Osman SIRAKAYA <osmansirakaya96@gmail.com>
2022-12-19 00:00:17 +0300

    CRUD operation with hibernate

--- hibernateDemo/src/com/hibernatedemo/Main.java
+++ hibernateDemo/src/com/hibernatedemo/Main.java
@@ -9,44 +9,80 @@ import org.hibernate.Session;
 
 
 public class Main {
-
 	public static void main(String[] args) {
+		selectData();
+		
+  }
+	
+	public static void selectData() {
 		SessionFactory factory = new Configuration()
 				.configure("hibernate.cfg.xml")
 				.addAnnotatedClass(City.class)
 				.buildSessionFactory();
-		Session session = factory.getCurrentSession();
-		
+		Session session = factory.getCurrentSession();		
 		try {
 			session.beginTransaction();
-			// HQL -> "Hibernate Query Languange"
-			// ("from City c where c.countryCode='TUR' AND c.district='Ankara'")
-			// ("from City c where c.name LIKE '%kar%'")
-//			List<City> cities = session.createQuery("select c.countryCode from City c GROUP  BY c.countryCode'").getResultList();// city tablosundan çek
-//			
-//			for(City city:cities)
-//				System.out.println(city.getName());
-			
-			// insert		
-//			  City city = new City(); 
-//			  city.setName("düzce 10"); 
-//			  city.setCountryCode("TUR");
-//			  city.setDistrict("karadeniz"); 
-//			  city.setPopulation("10000");
-//			  session.save(city);
-			
-			// update 
-			//City city = session.get(City.class, 4080); // id'si 4080 olan veriyi getir.
-			//city.setPopulation("90000");
-			//session.save(city);
-			
-			//delete
+			List<City> cities = session.createQuery("from City c where c.countryCode='TUR'").getResultList();// city tablosundan çek		
+			for(City city:cities)
+				System.out.println(city.getName());				
+		}finally {
+			factory.close();
+		}
+	}
+	
+	public static void insertData() {
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(City.class)
+				.buildSessionFactory();
+		Session session = factory.getCurrentSession();		
+		try {
+			session.beginTransaction();
+			  City city = new City(); 
+			  	city.setName("düzce 10"); 
+			  	city.setCountryCode("TUR");
+			  	city.setDistrict("karadeniz"); 
+			  	city.setPopulation("10000");
+			  session.save(city);
+			session.getTransaction().commit(); // işlem başarıyla bitince DB'ye basar
+			System.out.println("sehir eklendi");
+		}finally {
+			factory.close();
+		}
+	}
+	
+	public static void updateData() {
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(City.class)
+				.buildSessionFactory();
+		Session session = factory.getCurrentSession();		
+		try {
+			session.beginTransaction();
+			City city = session.get(City.class, 235760); //id'si 235760 olan veriyi getir.
+			city.setName("Ankara");	
+			session.getTransaction().commit(); 
+			System.out.println("sehir güncellendi");
+		}finally {
+			factory.close();
+		}		
+	}
+	
+	public static void deleteData() {
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(City.class)
+				.buildSessionFactory();
+		Session session = factory.getCurrentSession();	
+		try {
+			session.beginTransaction();
 			City city = session.get(City.class, 4080);
 			session.delete(city);			
-			session.getTransaction().commit(); // işlem bitince DB'ye basar
+			session.getTransaction().commit(); 
 			System.out.println("şehir silindi");
 		}finally {
 			factory.close();
 		}
-  }
+	}
+	
 }
