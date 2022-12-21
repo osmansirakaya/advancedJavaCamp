50033d9fb57d0967965ec6387600ccee76712ad7
Osman SIRAKAYA <osmansirakaya96@gmail.com>
2022-12-21 23:16:58 +0300

    first rest application

--- springBootDemo/src/main/java/com/javacourse/springBootDemo/apiDemo/ProductsController.java
+++ springBootDemo/src/main/java/com/javacourse/springBootDemo/apiDemo/ProductsController.java
@@ -1,5 +1,20 @@
 package com.javacourse.springBootDemo.apiDemo;
 
-public class ProductsController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class ProductsController {
+		
+		@GetMapping("/") // uygulama çalışınca bu gelmelidir
+		public String get() {
+			return "laptop";
+		}
+		
+		@GetMapping("/products") 
+		public String get2() {
+			return "laptop 3";
+		}
+		
+		
 }
