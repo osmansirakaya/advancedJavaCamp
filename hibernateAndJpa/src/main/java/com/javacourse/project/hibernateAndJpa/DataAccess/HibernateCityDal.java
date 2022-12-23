1c39418905fc5a7018a5395134fdf1d30aeef69c
Osman SIRAKAYA <osmansirakaya96@gmail.com>
2022-12-23 23:19:43 +0300

    data access layer

--- hibernateAndJpa/src/main/java/com/javacourse/project/hibernateAndJpa/DataAccess/HibernateCityDal.java
+++ hibernateAndJpa/src/main/java/com/javacourse/project/hibernateAndJpa/DataAccess/HibernateCityDal.java
@@ -1,48 +1,56 @@
 package com.javacourse.project.hibernateAndJpa.DataAccess;
-
 import java.util.List;
-
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
-
 import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.javacourse.project.hibernateAndJpa.Entities.City;
-// JPA - ORM 
+
+// JPA - bir ORM gibi ortaya çıkmıştır. daha sonra javada standart olmuştur 
+@Repository
 public class HibernateCityDal implements ICityDal{
 	
-	@Autowired // 
-	public HibernateCityDal(EntityManager entityManager) {
+	@Autowired // bu anatosyon, entityManager'ın implementasyonu nedir diye gerekli enjeksiyon yapacak
+	public HibernateCityDal(EntityManager entityManager) { // constructor 
 		this.entityManager = entityManager;
 	}
 
-	private EntityManager entityManager;
+	private EntityManager entityManager; // jpa'dan gelen session'a karşılık gelir
 	//AOP - Aspect Oriented Programming
 	@Override
 	@Transactional
 	public List<City> getAll() {
-		Session session = entityManager.unwrap(Session.class);
-		List<City> cities = session.createQuery("from City", City.class).getResultList();
+		Session session = entityManager.unwrap(Session.class); 
+		List<City> cities = session.createQuery("from City", City.class).getResultList(); // City class'ına map edecek
 		return cities;
 	}
 
 	@Override
 	public void add(City city) {
-		// TODO Auto-generated method stub
-		
+		Session session = entityManager.unwrap(Session.class); 
+		session.saveOrUpdate(city);		
 	}
 
 	@Override
 	public void update(City city) {
-		// TODO Auto-generated method stub
-		
+		Session session = entityManager.unwrap(Session.class); 
+		session.saveOrUpdate(city);		
 	}
 
 	@Override
 	public void delete(City city) {
-		// TODO Auto-generated method stub
-		
+		Session session = entityManager.unwrap(Session.class); 
+		City cityToDelete = session.get(City.class, city.getId());
+		session.delete(cityToDelete);	
 	}
 
+	@Override
+	public City getById(int id) {
+		Session session = entityManager.unwrap(Session.class); 
+		
+	City city = session.get(City.class, id);
+	return city;
+	}
 }
--- hibernateAndJpa/src/main/java/com/javacourse/project/hibernateAndJpa/DataAccess/ICityDal.java
+++ hibernateAndJpa/src/main/java/com/javacourse/project/hibernateAndJpa/DataAccess/ICityDal.java
@@ -1,13 +1,14 @@
 package com.javacourse.project.hibernateAndJpa.DataAccess;
+import java.util.List; 
+import com.javacourse.project.hibernateAndJpa.Entities.City; // City başka pakette olduğu için import ediyoruz
 
-import java.util.List;
-import com.javacourse.project.hibernateAndJpa.Entities.City;
 
-
-public interface ICityDal {
+public interface ICityDal { // veri erişim için temel interface 
 	
-	List<City> getAll();
+	List<City> getAll(); // şehirleri listelicek yapı
 	void add(City city);
 	void update(City city);
 	void delete(City city);
+	City getById(int id);
+
 }
